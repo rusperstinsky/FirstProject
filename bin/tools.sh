e02fe43f50590f851c55c23875848f90b56b1cd0
neogeo <benjamin.martinez05@gmail.com>
2016-10-04 19:20:44 -0500

    test

---
+++ bin/tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export GRADLE_HOME=/home/neogeo/FirstProject/herramientas/gradle
+export JAVA_HOME=/usr/local/lib/jdk1.7.0_79
+export ANT_HOME=/usr/local/lib/apache-ant-1.9.0
+
+export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$GRADLE_HOME/bin:$PATH
+export PATH=$ANT_HOME/bin:$PATH
+
